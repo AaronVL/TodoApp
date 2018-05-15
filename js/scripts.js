52588a67a38c8ef4c201179163d7afc74cc53576
aaron lucero <avlucero1121@gmail.com>
2018-05-14 20:57:35 -0700

    trying out event listeners

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+const todo = document.querySelector('#todo');
+const li = document.querySelector('li');
+const button = document.querySelector('.btn-default');
+
+button.addEventListener('click', (e) => {
+  let newTodo = document.createElement('li');
+  newTodo.textContent =
+})
