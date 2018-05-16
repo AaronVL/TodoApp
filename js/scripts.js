cd21aeda70606f23eaeb383cdd3f5f14735e6889
aaron lucero <avlucero1121@gmail.com>
2018-05-16 15:57:25 -0700

    got add button working

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,22 @@
-const todo = document.querySelector('#todo');
-const li = document.querySelector('li');
-const button = document.querySelector('.btn-default');
+const button = document.querySelector('#addButton');
+const listItem = document.querySelector('ul');
+
 
 button.addEventListener('click', (e) => {
-  let newTodo = document.createElement('li');
-  newTodo.textContent =
+  let li = document.createElement('li');
+  let taskValue = document.querySelector('#newTask').value;
+  let todo = document.createTextNode(taskValue);
+  li.appendChild(todo);
+  if (taskValue === '') {
+    alert("You must add a task");
+  } else {
+    document.querySelector('#todoList').appendChild(li);
+  }
+  document.querySelector('#newTask').value = "";
 })
+
+listItem.addEventListener('click', (e) => {
+  if (e.target.tagName === 'li') {
+    e.target.classList.toggle('lineThrough');
+}
+});
